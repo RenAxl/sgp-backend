298c2013a8c27243f90fe9e471f117833fa4c345
Renan Duarte <renandg.axl@gmail.com>
2025-01-20 20:49:25 -0300

    Terraform and Ansible infrastructure created

---
+++ infrastructure/terraform/modules/aws_sgp_infrastructure/main.tf
@@ -0,0 +1,135 @@
+resource "aws_security_group" "microservice_ec2_sg" {
+  name        = "${var.microservice_name}-ec2-sg"
+  description = "Allow traffic on ports 22 and 80"
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.microservice_name}"
+  }
+}
+
+resource "aws_instance" "microservice_ec2" {
+  ami                    = "ami-007855ac798b5175e"
+  instance_type          = "t2.micro"
+  key_name               = "sgp-ec2-key"
+  vpc_security_group_ids = [aws_security_group.microservice_ec2_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.s3_dynamodb_full_access_instance_profile.name
+
+  provisioner "local-exec" {
+    command = "sleep 60; ssh-keyscan ${self.public_ip} >> ~/.ssh/known_hosts"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${var.microservice_name} ansible_ssh_private_key_file=~/aws/key-pairs/sgp-ec2-key.pem id=${self.id} ansible_host=${self.public_ip} ansible_user=ubuntu aws_region=${var.region} aws_s3_bucket=${length(aws_s3_bucket.microservice_s3) > 0 ? aws_s3_bucket.microservice_s3[0].bucket : "N/A"} aws_dynamodb_table=${length(aws_dynamodb_table.microservice_dynamodb) > 0 ? aws_dynamodb_table.microservice_dynamodb[0].name : "N/A"} >> /etc/ansible/hosts"
+
+  }
+
+  provisioner "local-exec" {
+    command = "sed -i '/${self.id}/d' /etc/ansible/hosts"
+    when    = destroy
+  }
+
+
+  tags = {
+    Name = "${var.microservice_name}"
+  }
+}
+
+
+resource "aws_dynamodb_table" "microservice_dynamodb" {
+  count        = var.create_dynamodb ? 1 : 0
+  name         = "${var.microservice_name}-dynamodb"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "${var.microservice_name}"
+  }
+}
+
+resource "random_string" "bucket_suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket" "microservice_s3" {
+  count  = var.create_s3 ? 1 : 0
+  bucket = "${var.microservice_name}-s3-${random_string.bucket_suffix.result}"
+
+  tags = {
+    Name = "${var.microservice_name}"
+  }
+}
+
+resource "aws_iam_role" "s3_dynamodb_full_access_role" {
+  name = "${var.microservice_name}-s3_dynamodb_full_access_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+    Name = "${var.microservice_name}"
+  }
+
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full_access_role_policy_attachment" {
+  role       = aws_iam_role.s3_dynamodb_full_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_full_access_role_policy_attachment" {
+  role       = aws_iam_role.s3_dynamodb_full_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+
+}
+
+resource "aws_iam_instance_profile" "s3_dynamodb_full_access_instance_profile" {
+  name = "${var.microservice_name}-s3_dynamodb_full_access_instance_profile"
+  role = aws_iam_role.s3_dynamodb_full_access_role.name
+
+  tags = {
+    Name = "${var.microservice_name}"
+  }
+}
+
---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,43 @@
+variable "microservice_name" {
+  description = "Nome do microservice"
+  type        = string
+  default     = "sgp"
+}
+
+variable "create_dynamodb" {
+  description = "Define se a tabela DynamoDB deve ser criada"
+  type        = bool
+  default     = true
+}
+
+variable "create_s3" {
+  description = "Define se o bucket S3 deve ser criado"
+  type        = bool
+  default     = true
+}
+
+variable "region" {
+  description = "Nome da região da AWS"
+  default     = "us-east-1"
+}
+
+/*
+// EXEMPLO SE FOSSE CRIAR COMO LISTA
+variable "microservice_names" {
+  description = "A list of microservice names"
+  type        = list(string)
+  default     = ["authuser"]
+}
+
+variable "create_dynamodb" {
+  description = "Define se a tabela DynamoDB deve ser criada"
+  type        = list(bool)
+  default     = [false]
+}
+
+variable "create_s3" {
+  description = "Define se o bucket S3 deve ser criado"
+  type        = list(bool)
+  default     = [false]
+}
+*/
